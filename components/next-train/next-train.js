3517613fbb430677f607aa2c52d6497624578ade
Simon McManus <mcmanus.simon@gmail.com>
2014-10-09 22:14:57 +0100

    change

--- components/next-train/next-train.js
+++ components/next-train/next-train.js
@@ -1,7 +1,6 @@
 var template = require('./next-train.jade');
 
 exports.render = function(data) {
-    console.log('render train status', data);
     $('#nextTrain').html(template({ 'nextTrain': data }));
 };
 
