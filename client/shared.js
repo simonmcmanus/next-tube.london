4a5a2056724a67367a336e3fd87c9c5b185ded4f
Simon McManus <mcmanus.simon@gmail.com>
2014-10-09 22:24:43 +0100

    stuffs

--- client/shared.js
+++ client/shared.js
@@ -2,7 +2,7 @@ var nextBus = require('../components/next-bus/next-bus.js');
 var nextTrain = require('../components/next-train/next-train.js');
 var trainStatus = require('../components/train-status/train-status.js');
 
-var socket = io('http://localhost');
+var socket = io('http://woodford.today:80');
 
 socket.on('nextTrain', nextTrain.render);
 socket.on('trainStatus', trainStatus.render);
