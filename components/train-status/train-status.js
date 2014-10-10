decf3ad107103a8dae80b346d9786996f36c79f4
Simon McManus <mcmanus.simon@gmail.com>
2014-10-10 08:22:20 +0100

    broken tflstatus

--- components/train-status/train-status.js
+++ components/train-status/train-status.js
@@ -1,7 +1,6 @@
 var template = require('./train-status.jade');
 
 exports.render = function(data) {
-    console.log('render train status');
     $('#tflStatus').replaceWith(template({'tflStatus': data}));
 };
 
