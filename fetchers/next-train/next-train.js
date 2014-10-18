368f8b304ffbf501ec398e4678d09f4b89673e1e
Simon McManus <mcmanus.simon@gmail.com>
2014-10-18 12:39:28 +0100

    added station fetcher as socket.io rooms started to feel very coupled and not as simple as i had hoped.

---
+++ fetchers/next-train/next-train.js
@@ -0,0 +1,93 @@
+'use strict';
+var request = require('request');
+var async = require('async');
+var parseString = require('xml2js').parseString;
+
+var stationLister = require('./station-lister');
+
+var stationCodes = require('./stations.json');
+
+exports.get = function (stationCode, callback) {
+
+    var sortTrains = function (trains) {
+        if (!trains) {
+            return;
+        }
+        return trains.map(function (train) {
+            train = train.$;
+            return {
+                dueIn: train.TimeTo,
+                destination: train.Destination,
+                isStalled: (train.IsStalled === 1),
+                location: train.Location
+            };
+        });
+    };
+
+    request('http://cloud.tfl.gov.uk/TrackerNet/PredictionDetailed/C/' + stationCode, function (error, data) {
+        parseString(data.body, function (err, result) {
+            if (!result || !result.ROOT) {
+                return callback(true);
+            }
+            var platforms = result.ROOT.S[0].P;
+            var out = {
+                    code: stationCode,
+                    name: result.ROOT.S[0].$.N,
+                    trains: {}
+            };
+            Object.keys(platforms).forEach(function (platform) {
+                var direction = platforms[platform].$.N.split(' - ')[0];
+                var trains = sortTrains(platforms[platform].T);
+
+                if (!out.trains[direction]) {
+                    out.trains[direction] = [];
+                }
+                out.trains[direction].push.apply(out.trains[direction], trains);
+            });
+            callback(null, out);
+        });
+    });
+};
+
+exports.getAll = function (io, callback) {
+    // no io means no rooms which means not stations to check.
+    var stations = stationLister.getAllStations();
+    console.log('get stations', stations);
+    async.map(stations, exports.get, function (e, d) {
+        // convert to object.
+        var out = {
+            stationCodes: stationCodes,
+            stations: {}
+        };
+        d.forEach(function (item) {
+            out.stations[item.code] = item;
+        });
+        callback(e, out);
+    });
+};
+
+exports.checkForChanges = function (ds, cache, changeFound) {
+    for (var station in ds.nextTrain.stations) {
+        if (cache.nextTrain.stations[station] && ds.nextTrain.stations[station]) {
+            var oldTrains = cache.nextTrain.stations[station].trains;
+            var newTrains = ds.nextTrain.stations[station].trains;
+            if (JSON.stringify(oldTrains) !== JSON.stringify(newTrains)) {
+
+                // strip other trains from cache, needs to change.
+                var _stations = ds.nextTrain.stations;
+                var out = ds.nextTrain;
+                out.stations = {};
+                out.stations[station] = _stations[station];
+                changeFound(station, out);
+            }
+        } else if(ds.nextTrain.stations[station]) {
+            changeFound(station, ds.nextTrain.stations[station]);
+        }
+    }
+};
+
+exports.events = {
+    'next-train:station:listen:start':  stationLister.add,
+    'next-train:station:listen:stop': stationLister.remove,
+    'disconnect': stationLister.disconnect
+};
---
+++ fetchers/next-train/station-lister.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/**
+ * The fetcher needs an up to date list of stations to fetch.
+ *
+ * In order to do this we track when a user is added and removed from a room.
+ *
+ * I originally tried to replace this logic with socket.io room functionality but the list of active
+ * rooms was never clean becuase it adds every connected socket to its own room.
+ *
+ * This file also ensures lose coupling with socket.io.
+ */
+
+var _ = require('underscore');
+
+var  active = exports.active = {
+    stations: ['WFD'],
+    sessions: {
+        'WFD': {}
+    }
+};
+
+// remove the station from the keys,
+var deleteStation = function (stationId) {
+    // always check woodford so we have something upto date for first page load.
+    if (stationId !== 'WFD') {
+        active.stations = _.without(active.stations, stationId);
+    }
+};
+
+// does station have any active sessions listening.
+var isStationEmpty = function (station) {
+    if (!active.sessions[station]) { // if we dont know about the station
+        return true;
+    }
+    return (active.sessions[station] === {});
+};
+
+// stops polling the api for this station.
+var cleanupStation = function (sessionId, station) {
+    if (isStationEmpty(sessionId, station)) {
+        deleteStation(station);
+    }
+};
+
+exports.getAllStations = function () {
+    return active.stations;
+};
+
+// stop listening to a station for a session id.
+exports.remove = function (socket, station) {
+    var socketId = socket.id;
+    if (active.sessions[station]) { // not sure this check should be necessary.
+        delete active.sessions[station][socketId];
+    }
+    cleanupStation(socketId, station);
+};
+
+exports.disconnect = function (sessionId) {
+    var nextTrainUsers = active.sessions;
+    Object.keys(nextTrainUsers).forEach(function (station) {
+        if (nextTrainUsers[station][sessionId]) {
+            exports.remove(sessionId, station);
+        }
+        cleanupStation(sessionId, station);
+    });
+};
+
+exports.add = function (socket, stationId) {
+    var socketId = socket.id;
+    if (active.stations.indexOf(stationId) === -1) {
+        active.stations.push(stationId);
+    }
+    if (!active.sessions[stationId]) {
+        active.sessions[stationId] = {};
+    }
+    active.sessions[stationId][socketId] = true;
+};
