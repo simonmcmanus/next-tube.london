95e3739e4f800a19881117ba6ee8110d7aa8610e
Simon McManus <mcmanus.simon@gmail.com>
2014-10-27 08:45:09 +0000

    points class working on point

--- client/shared.js
+++ client/shared.js
@@ -24,7 +24,13 @@ var stationCodes = require('../fetchers/next-train/url-codes.json');
 page('/central-line/:stationName', function(context, next) {
     var code = stationCodes[context.params.stationName];
     // to emit stop listening.
+    $('li a.point').removeClass('point');
     $('#map-container').attr('data-station', code);
+    setTimeout(function() {
+        var selector = 'li.station.' + code + ' a';
+        console.log(selector);
+        $(selector).addClass('point');
+    }, 500);
     nextTrain.showLoader();
     nextTrain.getStationData(context.params.stationName, socket);
 });
