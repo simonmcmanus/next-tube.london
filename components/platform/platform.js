f17fb1a612d2e1ebada5e816b1950081533b080a
Simon McManus <mcmanus.simon@gmail.com>
2014-12-23 15:25:58 +0000

    about to "simplify" the html

--- components/platform/platform.js
+++ components/platform/platform.js
@@ -9,15 +9,14 @@ var train = require('../train/train');
 
 exports.init = function($el, bus) {
     train.init($el.find('.train'));
-    bus.on('WFD.trains.Westbound', newTrain.bind(null, $el));
-    bus.on('WFD.trains.Eastbound', newTrain.bind(null, $el));
+    bus.on('WFD.trains.Westbound', newTrain.bind(null, 'Westbound', $el));
+    bus.on('WFD.trains.Eastbound', newTrain.bind(null, 'Eastbound', $el));
     // bus.on('remove train', removeTrain);
 };
 
-function newTrain($el) 
-{
+function newTrain(direction, $el) {
+    $el.find('ul[data-direction=' + direction + ']').append($('<h1>hhiii</h1>'))
     debugger;
-    $el.append($('<h1>hhiii</h1>'))
 }
 
 function removeTrain() {
