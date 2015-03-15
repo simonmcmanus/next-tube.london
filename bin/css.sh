6e132548237f7b171da0010a43684dade553180c
Simon McManus <mcmanus.simon@gmail.com>
2015-03-15 16:10:27 +0000

    removing js from git

--- bin/css.sh
+++ bin/css.sh
@@ -5,8 +5,7 @@ mkdir ./public/pages/css/
 #rm -Rf .sass-cache
 
 
-./node_modules/node-sass/bin/node-sass /pages/shared/shared.scss ./public/pages/css/style.css
-
+./node_modules/node-sass/bin/node-sass  ./pages/shared/shared.scss ./public/pages/css/style.css
 ./node_modules/node-sass/bin/node-sass  ./pages/home/home.scss ./public/pages/css/home.css
 ./node_modules/node-sass/bin/node-sass  ./pages/about/about.scss ./public/pages/css/about.css
 ./node_modules/node-sass/bin/node-sass  ./pages/station/station.scss ./public/pages/css/station.css
