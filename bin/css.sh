ef1222ed56d18c223d0d466b02f5a3b3e6d56b16
Simon McManus <mcmanus.simon@gmail.com>
2015-03-15 16:03:16 +0000

    path test

--- bin/css.sh
+++ bin/css.sh
@@ -5,11 +5,11 @@ mkdir ./public/pages/css/
 #rm -Rf .sass-cache
 
 
-./node_modules/node-sass/bin/node-sass --source-map/pages/shared/shared.scss public/pages/css/style.css
+./node_modules/node-sass/bin/node-sass --source-map/pages/shared/shared.scss ./public/pages/css/style.css
 
-./node_modules/node-sass/bin/node-sass --source-map ./pages/home/home.scss public/pages/css/home.css
-./node_modules/node-sass/bin/node-sass --source-map ./pages/about/about.scss public/pages/css/about.css
-./node_modules/node-sass/bin/node-sass --source-map ./pages/station/station.scss public/pages/css/station.css
+./node_modules/node-sass/bin/node-sass --source-map ./pages/home/home.scss ./public/pages/css/home.css
+./node_modules/node-sass/bin/node-sass --source-map ./pages/about/about.scss ./public/pages/css/about.css
+./node_modules/node-sass/bin/node-sass --source-map ./pages/station/station.scss ./public/pages/css/station.css
 
 
 gulp autoprefixer
